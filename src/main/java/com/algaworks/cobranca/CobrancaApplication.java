b2d33318817f272a1f5611750e7ffbf51a354671
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-10 23:39:54 -0200

    Criando o protótipo HTML do projeto Enviando dados para o servidor

--- src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cobranca2Application {
+public class CobrancaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cobranca2Application.class, args);
+		SpringApplication.run(CobrancaApplication.class, args);
 	}
 }
--- src/test/java/com/algaworks/cobranca/CobrancaApplicationTests.java
+++ src/test/java/com/algaworks/cobranca/CobrancaApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Cobranca2ApplicationTests {
+public class CobrancaApplicationTests {
 
 	@Test
 	public void contextLoads() {
