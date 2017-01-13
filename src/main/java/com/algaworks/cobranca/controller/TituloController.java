858ab2dd932c9818894272c3f62ab03a3f8b7302
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-13 21:40:00 -0200

    Montando combobox dinamicamente

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.algaworks.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -17,8 +22,13 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		
+		mv.addObject("todosStatusTitulos", StatusTitulo.values());
+		
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -30,6 +40,13 @@ public class TituloController {
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		
 		return mv;
+		
+	}
+	
+	@ModelAttribute("todosStatusTitulos")
+	public List<StatusTitulo> todosStatusTitulo() {
+	
+		return Arrays.asList(StatusTitulo.values());
 		
 	}
 	
