99a93030404d5500559ad15901afe971dc0d7b18
Marcos Barbosa <barbosa.mg@gmail.com>
2017-01-15 23:43:48 -0200

    Corrigindo bug - ao cancelar a exclusão o id ia concatenando várias vezes

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,5 +1,6 @@
 $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	
+	
 	var button = $(event.relatedTarget);
 	
 	var codigoTitulo = button.data('codigo');
@@ -9,7 +10,7 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
 	
 	var form = modal.find('form');
 	
-	var action = form.attr('action');
+	var action = form.data('url-base');
 	
 	if(!action.endsWith('/')) {
 		action += '/';
